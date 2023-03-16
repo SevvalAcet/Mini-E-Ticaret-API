82133a7da763f7ae78a3bd31ebd20bd71e9870f2
SevvalAcet <83153151+SevvalAcet@users.noreply.github.com>
2023-03-16 23:32:47 +0300

    EF Core Tracking Performans Optimizasyonu

--- Core/ETicaretAPI.Aplication/Repositories/IReadRepository.cs
+++ Core/ETicaretAPI.Aplication/Repositories/IReadRepository.cs
@@ -5,9 +5,9 @@ namespace ETicaretAPI.Application.Repositories
 {
     public interface IReadRepository<T> : IRepository<T> where T : BaseEntity
     {
-        IQueryable<T> GetAll();
-        IQueryable<T> GetWhere(Expression<Func<T, bool>> method);
-        Task<T> GetSingle(Expression<Func<T, bool>> method);
-        Task<T> GetByIdAsync(String id);
+        IQueryable<T> GetAll(bool tracking = true);
+        IQueryable<T> GetWhere(Expression<Func<T, bool>> method, bool tracking = true);
+        Task<T> GetSingle(Expression<Func<T, bool>> method, bool tracking = true);
+        Task<T> GetByIdAsync(String id, bool tracking = true);
     }
 }
--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -19,13 +19,17 @@ namespace ETicaretAPI.API.Controllers
         [HttpGet]
         public async Task Get()
         {
-            await _productWriteRepository.AddRangeAsync(new()
-            {
-                new() {Id =Guid.NewGuid(), Name="Product1",Price=100,CreatedDate=DateTime.UtcNow,Stock=10},
-                new() {Id =Guid.NewGuid(), Name="Product2",Price=200,CreatedDate=DateTime.UtcNow,Stock=10},
-                new() {Id =Guid.NewGuid(), Name="Product3",Price=300,CreatedDate=DateTime.UtcNow,Stock=10},
-            });
-            var count = await _productWriteRepository.SaveAsync();
+            //await _productWriteRepository.AddRangeAsync(new()
+            //{
+            //    //new() {Id =Guid.NewGuid(), Name="Product1",Price=100,CreatedDate=DateTime.UtcNow,Stock=10},
+            //    //new() {Id =Guid.NewGuid(), Name="Product2",Price=200,CreatedDate=DateTime.UtcNow,Stock=10},
+            //    //new() {Id =Guid.NewGuid(), Name="Product3",Price=300,CreatedDate=DateTime.UtcNow,Stock=10},
+            //});
+            //var count = await _productWriteRepository.SaveAsync();
+
+            Product p = await _productReadRepository.GetByIdAsync("03df16cc-6642-49e0-b2b6-2cd762745610");
+            p.Name = "Mehmet";
+            await _productWriteRepository.SaveAsync();
         }
 
         [HttpGet("{id}")]
